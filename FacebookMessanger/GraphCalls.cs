5e37776393ef2274735de1aba6394fe541a1c4ec
Brian Norlaner <norla053@umn.edu>
2017-07-29 15:18:04 -0500

    Fixed the project, got rid of extra crap

---
+++ FacebookMessanger/GraphCalls.cs
@@ -0,0 +1,186 @@
+﻿using Facebook;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FacebookGraphAPI.Data;
+//using WebApiContrib.Formatting.JavaScriptSerializer;
+
+namespace FacebookGraphAPI
+{
+    class GraphCalls
+    {
+        const string pageId = "1130035050388269";
+        const string pageAuth = "EAACEdEose0cBAN6Yh4zAN8skcmREqgNRAnUbt9L7ZByD0408ZCPd2ZAv9dzD73PMvzLN1M9nQ3sZCbfWXNIntuSfZAfKV6Wkzggwly4BdHyqo2SGzLbjuVaxgP6H0dLUKeiYCLt9PpzBCr8pVrjE07muPMXL9or6u0nE535a6lbrtS1zw6td0O4pya7LfXLqrzZB1W8tZB54gZDZD";
+        public static Page getPage(FacebookClient client, int index)
+        {
+            var result = client.Get("me/accounts");
+            JObject pageListJson = JObject.Parse(result.ToString());
+            var resultData = pageListJson["data"][index];
+            Page page = JsonConvert.DeserializeObject<Page>(resultData.ToString());
+            return page;
+        }
+        public static List<Account> AllAccounts(FacebookClient client)
+        {
+            var GraphCall = "me/accounts";
+            var result = client.Get(GraphCall);
+            JObject accountListJson = JObject.Parse(result.ToString());
+            List<Account> accounts = new List<Account>();
+            int index = 0;
+            foreach(var account in accountListJson["data"])
+            {
+                Account x = new Account(account);
+                accounts.Add(x);
+                Console.WriteLine("Account # {0} : {1}", index, x.name);
+                index++;
+            }
+            return accounts;
+        }
+        public static List<Post> AllPosts(Page page)
+        {
+            FacebookClient client = new FacebookClient(page.access_token);
+
+            var GraphCall = string.Format("{0}/feed?limit=100", page.id);
+            var result = client.Get(GraphCall);
+            JObject postListJson = JObject.Parse(result.ToString());
+            // Create list of posts on a page
+            List<Post> posts = new List<Post>();
+            int index = 0;
+
+            // Assign all the fields of a post
+            foreach(var post in postListJson["data"])
+            {
+                Post x = new Post(post);
+                posts.Add(x);
+
+                Console.WriteLine("Post # {0}", index);
+                Console.WriteLine("story: {0}", x.story);
+                Console.WriteLine("message: {0}", x.message);
+                Console.WriteLine("created_time: {0}", x.created_time);
+                Console.WriteLine("id: {0}", x.id);
+                Console.WriteLine("**********************************");
+                index++;
+            }
+            return posts;
+        }
+        public static Post getPost(FacebookClient client, Page page, int postIndex)
+        {
+            var GraphCall = string.Format("{0}/feed?limit=100", page.id);
+            var result = client.Get(GraphCall);
+            JObject postListJson = JObject.Parse(result.ToString());
+            var postJToken = postListJson["data"][postIndex];
+            Post post = new Post(postJToken);
+            return post;
+        }
+        public static List<Comment>[] AllComments(Page page, List<Post> posts)
+        {
+            FacebookClient client = new FacebookClient(page.access_token);
+            List<Comment>[] allComments = new List<Comment>[posts.Count];
+            int index = 0;
+            foreach(var post in posts)
+            {
+                Console.WriteLine(index);
+                string graphCall = string.Format("{0}/comments?limit=1500", post.id);
+                var result = client.Get(graphCall);
+                JObject commentListJson = JObject.Parse(result.ToString());
+                List<Comment> comments = new List<Comment>();
+
+                // Assign all the fields of a comment
+                foreach(var comment in commentListJson["data"])
+                {
+                    Comment x = new Comment(comment);
+                    comments.Add(x);
+
+                    // Check if comment has replies
+                    try
+                    {
+                        graphCall = string.Format("{0}/comments", x.id);
+                        result = client.Get(graphCall);
+                        commentListJson = JObject.Parse(result.ToString());
+                        List<Comment> replyComments = new List<Comment>();
+                    }
+                    catch(Exception)
+                    {
+                        // comments has no replies
+                    }
+                }
+                allComments[index] = comments;
+                index++;
+            }
+            return allComments;
+        }
+        public static List<Comment> getComments(FacebookClient client, Post post)
+        {
+            string GraphCall = string.Format("{0}/comments?limit=1500", post.id);
+            var result = client.Get(GraphCall);
+            JObject commentListJson = JObject.Parse(result.ToString());
+            List<Comment> comments = new List<Comment>();
+            foreach(var comment in commentListJson["data"])
+            {
+                Comment x = new Comment(comment);
+                comments.Add(x);
+                GraphCall = string.Format("{0}/comments", x.id);
+                var replies = client.Get(GraphCall);
+                var repliesListJson = JObject.Parse(replies.ToString());
+                foreach(var reply in repliesListJson["data"])
+                {
+                    if (reply == null) break;
+                    Comment y = new Comment(reply);
+                    comments.Add(y);
+                }
+            }
+            return comments;
+        }
+        public static void PostPage(Page page, string message)
+        {
+            FacebookClient client = new FacebookClient(page.access_token);
+            dynamic messagePost = new ExpandoObject();
+            messagePost.access_token = page.access_token;
+            messagePost.caption = "THIS IS A NEW POST";
+            messagePost.message = message;
+
+            try { var result = client.Post(page.id + "/feed", messagePost); }
+            catch (FacebookOAuthException ex) { /* handle something */ }
+        }
+        public static void sendMessages(FacebookClient client, List<Comment> comments, string message, int messageIndex)
+        {
+            string GraphCall;
+            string messageCall;
+            string[] name;
+            dynamic messageObject = new ExpandoObject();
+            messageObject.message = string.Empty;
+            
+            Dictionary<string, bool> idArray = new Dictionary<string, bool>();
+            for (int i = 0; i < comments.Count; i++)
+            {
+                try
+                {
+                    idArray.Add(comments[i].from_id, false);
+                } catch(Exception) { /* from_id already added */ }
+            }
+
+            int index = 0;
+            foreach (var comment in comments)
+            {
+                if (messageIndex > index) { index++; continue; }
+                if (!idArray[comment.from_id])
+                {
+                    name = comment.from_name.Split(' ');
+                    messageCall = "Congratulations " + name[0] + message;
+                    messageObject.message = messageCall;
+                    GraphCall = string.Format("{0}/private_replies", comment.id);
+                    try
+                    {
+                        client.Post(GraphCall, messageObject);
+                        idArray[comment.from_id] = true; // set from_id to true after message is sent
+                    } catch (Exception) { /* message not sent */ }
+                }
+                index++;
+            }
+        }
+    }
+}
---
+++ FacebookMessanger/Program.cs
@@ -0,0 +1,101 @@
+﻿using Facebook;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static FacebookGraphAPI.Data;
+
+namespace FacebookGraphAPI
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            // Tyler's login credentials
+            // vyler.togt@gmail.com
+            // Mushroomcowmoon1 
+            /**************************************************************************
+             * STEP 1
+             * RUN AllAccounts
+             * SET accounIndex
+             * 
+             * STEP 2
+             * RUN AllPosts to see which post you want to use
+             * SET postIndex
+             * 
+             * STEP 3
+             * SET message to you're custom message.
+             * check GraphCalls.sendMessages for how message is created with user name
+             * 
+             * STEP 4 (optional)
+             *  Run GetMessages and find the messageIndex you wish to start at
+             * SET messageIndex
+             * 
+             * STEP 5
+             * Run program!
+             *
+             ****************************************************************************/
+
+            string message = "! You won our Pokemon Keychain Giveaway! Thank you for engaging in our community. Our friends over at Movie Treasures are sponsoring this event and want to give you 3 FREE keychains of your choice! \n\n" +
+                "How to Claim Your Prize: \n\n" +
+                "1. Follow Our Link >\n" +
+                "https://movietreasures.org/collections/anime-lovers/products/premium-quality-pokemon-keychains \n\n" +
+                "2. Add Any 3 Keychains to Your Cart \n\n" +
+                "3. Enter Coupon Code 'WINNER5' at Checkout \n\n" +
+                "4. Hurry! We Can Only Secure Your Exclusive Offer For 24 Hours! \n\n" +
+                "** Checkout the Movie Treasures FAQs page for answers to common questions \n\n" +
+                "https://movietreasures.org/pages/faqs-2 \n\n" +
+                "** Free shipping available in select regions only, all rates are discounted, but ultimately determined by location";
+
+            int accountIndex = 2;
+            int postIndex = 14;
+            int messageIndex = 0;
+
+            string Token = "EAAGsVkZAMInsBACm7nZCoCEV0bko7EUbJcBX1KsSC5m8azC9UVEWuYdVR0WtZBqg3H6nUu2Sy9JSECZC0OGv6KNCHTdgDcZAkcbrrOKiJfhVjxkv2hRvZCibn3cKZC5O65QYc0XAFrpYReuoZApPSqqtUcKSE0Be5tdOHohSQBGFjQZDZD";
+
+            var client = new FacebookClient(Token);
+            Page page = GraphCalls.getPage(client, accountIndex);
+            GraphCalls.AllAccounts(client);
+            List<Post> posts = GraphCalls.AllPosts(page);
+
+            SendMessages(Token, accountIndex, postIndex, message, messageIndex);
+        }
+        public static void SendMessages(string Token, int accountIndex, int postIndex, string message, int messageIndex)
+        {
+            var client = new FacebookClient(Token);
+            Page page = GraphCalls.getPage(client, accountIndex);
+            var pageClient = new FacebookClient(page.access_token);
+            Post post = GraphCalls.getPost(pageClient, page, postIndex);
+            List<Comment> comments = GraphCalls.getComments(pageClient, post);
+            GraphCalls.sendMessages(pageClient, comments, message, messageIndex);
+        }
+        public static string BrianGetAccessToken()
+        {
+            var client = new FacebookClient();
+            dynamic result = client.Get("oauth/access_token", new
+            {
+                client_id = "484701718548881",
+                client_secret = "b3ecb63d467656d404b892498bd3c5ed",
+                grant_type = "fb_exchange_token",
+                fb_exchange_token = "EAAG41WnBkZAEBAJBatZAAORY1k91hwedHwDYPQKJwR15304PVB0tjMWZArEMJHOZBJVVSxorZC0x24i3OcR5kKuqldwUJ8xK5vIG2xG1KI9ZBXpOulXRUSmhCKUfyheXpDVg12xnIAeESc1qWHeLCdDtJZBp6pWyPgRVwLrig18ZCRHMo3xpMMLGSlxwYw2ZBjI8ZD"
+            });
+            return result.access_token;
+        }
+        public static string TylerGetAccessToken()
+        {
+            var client = new FacebookClient();
+            dynamic result = client.Get("oauth/access_token", new
+            {
+                client_id = "470961523270267",
+                client_secret = "c8ae181332d6e4d996456bd9d2368480",
+                grant_type = "fb_exchange_token",
+                fb_exchange_token = "EAAGsVkZAMInsBABG2u9lsCoGlKVxHUxk0hcEG76y7n3odCICQIWPxxg9TvxMDoZCTcZBZASucGmn2QPMA5Amgqqh0D2KFoVY0icQsyig0yADQqFOKFwSOsZBjIPpJyhVFztFYVKXfaSBB0nt2YsQXZAZAm7bus4xcnSZCDzlYORnUPUQySn09bR7uo53YS2Uu4wZD"
+            });
+            return result.access_token;
+        }
+    }
+}
